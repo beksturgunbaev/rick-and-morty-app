dd0b0d4fc843e49e630ddff0480aecfe74f4f26b
beks <turgunbaaev@gmail.com>
2026-05-01 16:21:30 +0600

    Добавил мета тег для адаптации под моб устройства

--- src/pages/episodes/index.tsx
+++ src/pages/episodes/index.tsx
@@ -2,7 +2,7 @@ import { EpisodeCard } from "@/entities/episodes/ui/edisodeCard"
 
 const EpisodesPage = () => {
     return (
-        <div className="container mx-auto py-5">
+        <div className="container mx-auto py-5 px-3">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 <EpisodeCard name='Pilot' airDate='December 2, 2013' episodeCode='S01E01' />
             </div>
